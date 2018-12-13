3188ff2c2f9c2c80533d4eac37b0829d8d3c8638
chenjianjun <chenjianjun@lancai.cn>
2018-12-13 16:59:01 +0800

    v1.0

--- carrot_test.go
+++ carrot_test.go
@@ -21,10 +21,12 @@ var r3 = func() {}
 var o3 = func() {}
 
 var t4 = func() int { return 4 }
-var r4 = func() int { fmt.Println(""); return 5 }
+var r4 = func() int { fmt.Printf(""); return 5 }
 var o4 = func() int { return 6 }
 
 func TestSample(t *testing.T) {
+	carrot.UnpatchAll()
+
 	carrot.Patch(t1, r1, o1)
 	assert.Equal(t, 2, t1())
 	assert.Equal(t, 1, o1())
@@ -34,6 +36,8 @@ func TestSample(t *testing.T) {
 }
 
 func TestComplex(t *testing.T) {
+	carrot.UnpatchAll()
+
 	var b = [2000]byte{0}
 	assert.True(t, carrot.Patch(t2, r2, o2))
 	assert.Equal(t, 2, t2(b))
@@ -41,7 +45,38 @@ func TestComplex(t *testing.T) {
 	assert.Equal(t, 1, t2(b))
 }
 
+func TestIsolate(t *testing.T) {
+	carrot.UnpatchAll()
+
+	assert.True(t, carrot.Patch(t1, r1, o1))
+	assert.Equal(t, 2, t1())
+	assert.Equal(t, 2, r1())
+	assert.Equal(t, 1, o1())
+
+	assert.True(t, carrot.Patch(t4, r4, o4))
+	assert.Equal(t, 5, t4())
+	assert.Equal(t, 5, r4())
+	assert.Equal(t, 4, o4())
+
+	carrot.Unpatch(t4)
+
+	assert.Equal(t, 2, t1())
+	assert.Equal(t, 2, r1())
+	assert.Equal(t, 1, o1())
+
+	assert.Equal(t, 4, t4())
+	assert.Equal(t, 5, r4())
+	assert.Equal(t, 6, o4())
+
+	assert.True(t, carrot.Patch(t4, r4, o4))
+	assert.Equal(t, 5, t4())
+	assert.Equal(t, 5, r4())
+	assert.Equal(t, 4, o4())
+}
+
 func TestMulti(t *testing.T) {
+	carrot.UnpatchAll()
+
 	assert.True(t, carrot.Patch(t3, r3, o3))
 	assert.False(t, carrot.Patch(t3, r3, o3))
 	assert.Panics(t, func() { carrot.Patch(t1, r2, o1) })
@@ -63,4 +98,22 @@ func TestMulti(t *testing.T) {
 	assert.Equal(t, 1, t1())
 	assert.Equal(t, 2, r1())
 	assert.Equal(t, 3, o1())
+
+	assert.Equal(t, 2, t4())
+	assert.Equal(t, 2, r1())
+	assert.Equal(t, 4, o4())
+}
+
+var rPrintf = func(format string, a ...interface{}) (n int, err error) { n = 5555; return }
+var oPrintf = func(format string, a ...interface{}) (n int, err error) { return }
+
+func TestPrintf(t *testing.T) {
+	carrot.UnpatchAll()
+	assert.True(t, carrot.Patch(fmt.Printf, rPrintf, oPrintf))
+
+	assert.Equal(t, 5555, (func() int { n, _ := fmt.Printf(""); return n })())
+	assert.Equal(t, 0, (func() int { n, _ := oPrintf(""); return n })())
+
+	carrot.Unpatch(fmt.Printf)
+	assert.Equal(t, 0, (func() int { n, _ := fmt.Printf(""); return n })())
 }
